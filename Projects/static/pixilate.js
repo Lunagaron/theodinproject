32b584be95bdb39e946a0bbee14c3671ae9515a9
ArigatouGozaimaster <modelsine@gmail.com>
2023-01-31 19:58:29 +1100

    Updated projects with pixilate repo

---
+++ Projects/static/pixilate.js
@@ -0,0 +1,37 @@
+/*
+ * Pixilate is a webpage sketchpad made with HTML, CSS, and JavaScript.
+ * The files associated with this program include pixilate.css, pixilate.html
+ * and pixilate.js (this file).
+ */
+
+// Initalize Code when all content is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  /* Create a dynamically changing grid-size displayed on .html with the slider.
+   * Function createGrid(size) takes the current size of gridSizeSlider and
+   * resizes the grid displayed.
+   */
+
+  // Initialize constant variables
+  const gridContainer = document.getElementById("grid-container");
+  const gridSizeSlider = document.getElementById("grid-size");
+
+  // Create grids to be displayed
+  function createGrid(size) {
+    gridContainer.innerHTML = "";
+    for (let i = 0; i < size * size; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("grid-cell");
+      gridContainer.appendChild(cell);
+    }
+    document.documentElement.style.setProperty("--size", size);
+  }
+
+  // Automatically updates grid based on slider
+  gridSizeSlider.addEventListener("input", () => {
+    createGrid(gridSizeSlider.value);
+  });
+
+  // Loads grids on startup / page refresh
+  createGrid(gridSizeSlider.value);
+  /* End of DOMContentLoaded */
+});
