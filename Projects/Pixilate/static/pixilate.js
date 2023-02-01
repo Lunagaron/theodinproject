4ed2aeaf92f9cbb72bc22819e523e73bc34fead9
ArigatouGozaimaster <modelsine@gmail.com>
2023-02-01 18:48:34 +1100

    Include rainbow mode and erasing functionality

--- Projects/Pixilate/static/pixilate.js
+++ Projects/Pixilate/static/pixilate.js
@@ -14,18 +14,50 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initialize constant variables
   const gridContainer = document.getElementById("grid-container");
   const gridSizeSlider = document.getElementById("grid-size");
+  const penColorChanger = document.getElementById("pen-color");
+  const eraserCheck = document.getElementById("eraser");
+  const rainbowMode = document.getElementById("rainbowmode");
 
   /* Function createGrid uses a simple if loop to create the amount of individual cells required
-   *  to fill the page with grids */
+   * to fill the page with grids */
   function createGrid(size) {
-    gridContainer.innerHTML = "";
+    gridContainer.innerHTML = ""; // Set the elements inside gridContainer as an empty string
     for (let i = 0; i < size * size; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("grid-cell");
-      // Begin implementation of drawing functionality
-      cell.addEventListener("click", () => {
-        mouseClick(cell);
-      }); // End implementation of drawing functionality
+      const cell = document.createElement("div"); // Creates a constant variable "cell" as a div element
+      cell.classList.add("grid-cell"); // Updates cell's classList property
+      /*
+       * Begin implementation of drawing functionality (with color)
+       * Three event listeners are added to the "cell" variable
+       * mousedown - when the mouse button is pressed on the cell, the mouseclick property is activated
+       * mouseenter - when the mouse enters a cell, it checks if the mousedown property is active
+       * mouseup - resets the mousedown variable to false
+       */
+      cell.addEventListener("mousedown", () => {
+        mouseDown = true;
+        if (eraserCheck.checked) {
+          eraseClick(cell);
+        } else if (rainbowMode.checked) {
+          rainbowClick(cell);
+        } else {
+          mouseClick(cell);
+        }
+      });
+
+      cell.addEventListener("mouseenter", () => {
+        if (mouseDown) {
+          if (eraserCheck.checked) {
+            eraseClick(cell);
+          } else if (rainbowMode.checked) {
+            rainbowClick(cell);
+          } else {
+            mouseClick(cell);
+          }
+        }
+      });
+
+      cell.addEventListener("mouseup", () => {
+        mouseDown = false;
+      });
       gridContainer.appendChild(cell);
     }
     // Creates global variable "--size" for css
@@ -47,14 +79,37 @@ document.addEventListener("DOMContentLoaded", () => {
     ).innerHTML = `Current Canvas Resolution : ${gridSizeSlider.value}px`;
   });
 
-  /* Fetches pen colour and draws onto the canvas */
-
-  // Initialize variables
-  const penColorChanger = document.getElementById("pen-color");
-
   /* Function to change grid cell colour to penColour on click */
   function mouseClick(item) {
     item.style.backgroundColor = penColorChanger.value;
+  }
+
+  /* Function to revert cells back to original color */
+  function eraseClick(item) {
+    item.style.backgroundColor = "rgb(245, 245, 245)";
+  }
+
+  /* Functions to implement rainbow mode
+   * getRandomColors() returns three colors in the RGB spectrum
+   * rainbowClick() changes grid cell color to a random rainbow color
+   */
+  function getRandomColors() {
+    const colors = [0, 255, 127];
+    let result = [];
+    for (let i = 0; i < 3; i++) {
+      let randomIndex = Math.floor(Math.random() * colors.length);
+      let color = colors[randomIndex];
+      result.push(color);
+      if (result.filter((c) => c === color).length >= 2) {
+        colors.splice(randomIndex, 1);
+      }
+    }
+    return result;
+  }
+
+  function rainbowClick(item) {
+    const colors = getRandomColors();
+    item.style.backgroundColor = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
   }
 
   /* End of DOMContentLoaded */
